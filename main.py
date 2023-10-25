752b5c68e0b2f2d2220629ede0c9fcdf0083d2a9
lililusvardi <lilianalusvardi@ufl.edu>
2023-10-25 12:06:32 -0400

    Lili Lusvardi

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-# this is my first github addition
+def main_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
 
-print("hello world")
+def encode(password):
+    result = ""
+    for index in password:
+        num = int(index) + 3
+        result += str(num)
+    return result
+keepRunning = True
+while keepRunning:
+    main_menu()
+    choice = input("Please enter an option: ")
+    if choice == 1:
+        password = input("Please enter your password to encode:")
+        encoded = encode(password)
+        print("Your password has been encoded and stored\n")
+        main_menu()
+    if choice == 2:
+        
+    if choice == 3:
+        break
